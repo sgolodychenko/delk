bdfdf16b35d5bdef3561edd5e0082ac361ed7ab5
sgolodychenko <golodychenko@gmail.com>
2016-07-15 19:04:34 +0300

    constructor added

--- google-tests/src/test/java/com/delk/app/GoogleTests.java
+++ google-tests/src/test/java/com/delk/app/GoogleTests.java
@@ -8,6 +8,9 @@ import static com.codeborne.selenide.Selenide.open;
  * Created by Serhiy_Golodychenko on 7/15/2016.
  */
 public class GoogleTests {
+    public GoogleTests() {
+    }
+
     @Test
     public void userCanSearchKeywordWithGoogle() throws Exception {
         open("http://google.com");
