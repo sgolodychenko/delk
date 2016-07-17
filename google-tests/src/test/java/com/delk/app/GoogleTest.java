aa1540ce05871c821c174fd4a3b1b77ae9f94354
sgolodychenko <golodychenko@gmail.com>
2016-07-17 15:40:08 +0300

    google fix 3

--- google-tests/src/test/java/com/delk/app/GoogleTest.java
+++ google-tests/src/test/java/com/delk/app/GoogleTest.java
@@ -4,19 +4,21 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 /**
  * Created by Serhiy_Golodychenko on 7/15/2016.
  */
-public class GoogleTests {
+public class GoogleTest {
     @Test
     public void userCanSearchKeywordWithGoogle() throws Exception {
         System.setProperty("webdriver.chrome.driver", "D:\\Projects\\delk\\chromedriver\\chromedriver.exe");
         open("https://www.google.com.ua/");
         $(By.name("q")).setValue("delk").pressEnter();
         //$("#ires li.g").shouldHave(text("Wikipedia, the free encyclopedia"));
-        //$$("#ires li.g").shouldHave(size(10));
+        $$("#ires .g").shouldHave(size(10));
     }
 }
