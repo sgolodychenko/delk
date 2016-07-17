58b3e9f69e2d5148549a2107fca86cbc9b8200f9
sgolodychenko <golodychenko@gmail.com>
2016-07-17 15:04:29 +0300

    google fix 3

--- google-tests/src/test/java/com/delk/app/GoogleTests.java
+++ google-tests/src/test/java/com/delk/app/GoogleTests.java
@@ -1,8 +1,11 @@
 package com.delk.app;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.*;
+
 
 /**
  * Created by Serhiy_Golodychenko on 7/15/2016.
@@ -12,6 +15,8 @@ public class GoogleTests {
     public void userCanSearchKeywordWithGoogle() throws Exception {
         System.setProperty("webdriver.chrome.driver", "D:\\Projects\\delk\\chromedriver\\chromedriver.exe");
         open("https://www.google.com.ua/");
-
+        $(By.name("q")).setValue("delk").pressEnter();
+        //$("#ires li.g").shouldHave(text("Wikipedia, the free encyclopedia"));
+        //$$("#ires li.g").shouldHave(size(10));
     }
 }
